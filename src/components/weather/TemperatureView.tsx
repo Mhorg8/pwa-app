35ea4b2482340e99d40839851cc23cc452b4f70a
mohammad <mohammadalirezaie081@gmail.com>
2025-03-30 22:16:28 +0330

    create functioality for getting weather app

--- src/components/weather/TemperatureView.tsx
+++ src/components/weather/TemperatureView.tsx
@@ -1,16 +1,28 @@
-const TemperatureView = () => {
+import { Weather } from "../../types";
+
+interface Props {
+  weather?: Weather;
+}
+
+const TemperatureView = ({ weather }: Props) => {
+  if (!weather) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="flex flex-col items-center justify-center border-y border-stone-400 py-10">
       <div className="flex items-center h-fit ">
         <h1 className="text-9xl md:text-[150px] font-light text-dark-gray">
-          75
+          {weather.main.temp}
         </h1>
         <div className="flex h-full min-h-38 flex-col   items-center justify-between text-4xl ">
           <p className="">&deg;</p>
-          <p>F</p>
+          <p>C</p>
         </div>
       </div>
-      <p className="text-2xl tracking-wide font-light">MOSTLY SUNNY</p>
+      <p className="text-2xl tracking-wide font-light">
+        {weather.weather[0].description}
+      </p>
     </div>
   );
 };
--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -2,6 +2,8 @@ import TemperatureView from "../components/weather/TemperatureView";
 import SearchLocation from "../components/weather/SearchLocation";
 import { useEffect, useState } from "react";
 import { IoLocationOutline } from "react-icons/io5";
+import { Weather } from "../types";
+import { useSearchParams } from "react-router-dom";
 
 interface Location {
   city: null | string;
@@ -11,17 +13,23 @@ interface Location {
 
 const WeatherPage = () => {
   const [error, setError] = useState<string | null>(null);
+  const [weather, setWeather] = useState<Weather | null>(null);
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("location");
+
   const [location, setLocation] = useState<Location>({
     city: null,
     country: null,
     loc: null,
   });
 
+  // Fetch current location
   async function getCurrentLocation() {
     try {
       const response = await fetch(
         `https://ipinfo.io/json?token=8461a4291e36f7`
       );
+      if (!response.ok) throw new Error("Failed to fetch location");
       const data = await response.json();
       setLocation({
         city: data.city,
@@ -30,18 +38,30 @@ const WeatherPage = () => {
       });
     } catch (err) {
       console.error(err);
-      setError("Something went wrong. Pls try again.");
+      setError("Something went wrong. Please try again.");
     }
   }
 
+  // Fetch weather details
   async function getWeatherDetail() {
-    if (location.loc) {
-      const [lat, lon] = location.loc.split(",");
+    try {
+      if (location.loc || query) {
+        const [lat, lon] = location.loc ? location.loc.split(",") : [];
+        const API_KEY = "1399b4e72604b09f14d6522be3095722";
+        const getWeatherByCity = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`;
+        const getWeatherByCurrentLocation = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-      const response = await fetch(`
-        https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=1399b4e72604b09f14d6522be3095722`);
-      const data = await response.json();
-      console.log(data);
+        const response = await fetch(
+          query ? getWeatherByCity : getWeatherByCurrentLocation
+        );
+
+        if (!response.ok) throw new Error("Failed to fetch weather data");
+        const data = await response.json();
+        setWeather(data);
+      }
+    } catch (err) {
+      console.error(err);
+      setError("Unable to fetch weather data. Please try again.");
     }
   }
 
@@ -50,37 +70,51 @@ const WeatherPage = () => {
   }, []);
 
   useEffect(() => {
-    if (location) {
+    if (location.city || query) {
       getWeatherDetail();
     }
-  }, [location]);
+  }, [location, query]);
+
+  if (!weather) {
+    <div>LOading</div>;
+  }
 
   return (
     <div className="wrapper">
       <div className="flex flex-col items-center justify-center gap-y-10">
         <SearchLocation />
-        <div>
-          <div className="flex items-center gap-1">
+        <div className="">
+          <div className="flex  items-center gap-1">
             <span className="text-sm text-dark-gray">Current location</span>
             <IoLocationOutline />
           </div>
           <h3 className="text-4xl font-light tracking-wider">
-            {location.city}
+            {query ? query : location.city || "Fetching location..."}
           </h3>
 
           {error && <p className="text-red-500">{error}</p>}
         </div>
-        <TemperatureView />
+        <TemperatureView weather={weather} />
 
-        <div className="flex items-center justify-center gap-10">
-          <div className="flex flex-col items-center justify-center gap-3">
-            <h5 className="text-2xl font-light tracking-wide">TODAY</h5>
-            <div className="bg-red-200 h-10 w-10 "></div>
-            <div className="flex items-center gap-3 text-2xl font-light tracking-wide">
-              <p>77</p> | <p>54</p>
+        {weather && (
+          <div className="flex items-center justify-center gap-10">
+            <div className="flex flex-col items-center justify-center gap-3">
+              <h5 className="text-2xl font-light tracking-wide">TODAY</h5>
+              <div className="bg-red-200 h-10 w-10 "></div>
+              <div className="flex items-center gap-3 text-2xl font-light tracking-wide">
+                <div className="flex items-end font-light">
+                  <p>{weather.main.temp_min.toFixed(0)}</p>{" "}
+                  <span className="text-base">°C</span>
+                </div>
+                |
+                <div className="flex items-end font-light">
+                  <p>{weather.main.temp_max.toFixed(0)}</p>{" "}
+                  <span className="text-base">°C</span>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
