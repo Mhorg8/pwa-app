79f808c1339514e0a8c547fad018adf9be89f67d
mohammad <mohammadalirezaie081@gmail.com>
2025-04-10 23:27:53 +0330

    create header and footer

--- art-easy/src/layout.tsx
+++ art-easy/src/layout.tsx
@@ -1,11 +1,13 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer/Footer";
 
 export const LayoutPage = () => {
   return (
     <div>
-      <header>LayoutPage</header>
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
